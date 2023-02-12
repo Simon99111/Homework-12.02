c79659b6c9f495146175b5ba270ef668d25209b6
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-12 21:20:44 +0300

    Программа 10

---
+++ 10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99 && n < 1000)
+{
+    n = n % 100 / 10;
+    Console.WriteLine($"Вторая цифра числа = {n}");
+}
+else
+    Console.WriteLine("Вы ввели не трехзначное число");
