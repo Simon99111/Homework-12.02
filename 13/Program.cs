ee346642e6bba9bb02742e28ec001d51dd3b200f
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-12 22:07:46 +0300

    Программа 13

---
+++ 13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+if (k > 99)
+{
+    while (k > 999)
+    {
+        k = k / 10;
+    }
+    k = k % 10;
+    Console.WriteLine($"Третья цифра числа = {k}");
+}
+else
+    Console.WriteLine("Третьей цифры нет");
