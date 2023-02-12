e28dce3d953f868055af748cfe87371b287a1f26
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-12 22:25:51 +0300

    Программа 15

---
+++ 15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите цифру");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > -1 && n < 10)
+{
+    if (n == 6 || n == 7) Console.WriteLine("Да");
+    else
+        Console.WriteLine("Нет");
+}
+else
+    Console.WriteLine("Вы ввели не цифру");
